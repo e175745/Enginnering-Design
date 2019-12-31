570d1ddc120de4687cdef53de7b5452bcc838b20
Kazuma M <e175745@ie.u-ryukyu.ac.jp>
2019-12-31 22:49:06 +0900

    change 175745J's Fishing Demo

--- 175745J/Fishing_Demo/MainDisplay.swift
+++ 175745J/Fishing_Demo/MainDisplay.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class Main: UIViewController {
-
+    
+    @IBOutlet weak var TitleView: UIImageView!
+    
+    var title_image = UIImage(named:"Title")!
+    var clear_image = UIImage(named:"Clear")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 背景色設定
+        view.backgroundColor=UIColor(red: 0/255, green: 255/255, blue: 255/255, alpha: 1)
+        
+        // BGM設定
+        let soundUrl = Bundle.main.url(forResource: "Menu_BGM", withExtension: "mp3")
+        var soundID: SystemSoundID = 5
+        AudioServicesCreateSystemSoundID(soundUrl! as CFURL, &soundID)
+        AudioServicesPlaySystemSoundWithCompletion(soundID) {}
+        
+        // タイトル画像設定
+        TitleView.image = title_image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func DeleteImage(_ sender: Any) {
+        TitleView.image = clear_image
     }
-    */
-
+    
+    @IBAction func SetImage(_ sender: Any) {
+        TitleView.image = title_image
+    }
+    
 }
--- 175745J/Fishing_Demo/ViewController.swift
+++ 175745J/Fishing_Demo/ViewController.swift
@@ -9,153 +9,21 @@
 import UIKit
 import SceneKit
 import ARKit
-import CoreMotion
-
-
-//変面けんち
-//DeviceMotion
-
-class UIController: ViewController{
-    // カメラポジションを返す関数
-    func cameraNode(_ session: ARSession, didUpdate frame: ARFrame) -> SCNNode {
-        if let camera = sceneView.pointOfView { // カメラを取得
-            /*
-            // カメラの向いてる方向を計算
-            let mat = camera.transform
-            let cameraPosition = SCNVector3(mat.m31, mat.m32, mat.m33)
-            return cameraPosition
-            */
-            return camera
-        }
-        return SCNNode()
-    }
-    
-    func sendCamera()->SCNNode{
-        return camera
-    }
-    
-    func deviceAccelarate()->SCNVector3{
-        return deviceAcc
-    }
-    
-    func deviceRotation()->SCNVector3{
-        return deviceRot
-    }
-    
-    // 平面検知する関数
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if let planeAnchor = anchor as? ARPlaneAnchor {
-        let planeNode = PlaneNode(anchor: planeAnchor)
-           DispatchQueue.main.async(execute: {
-               node.addChildNode(planeNode)
-           })
-        }
-       }
-       
-       // 更新されたとき
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-           DispatchQueue.main.async(execute: {
-               // 平面ジオメトリのサイズを更新
-               if let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = node.childNodes[0] as? PlaneNode {
-                   // ノードの位置及び形状を修正する
-                   planeNode.update(anchor: planeAnchor)
-               }
-           })
-       }
-   
-}
-
-class PlaneNode: SCNNode{
-    fileprivate override init() {
-        super.init()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    init(anchor: ARPlaneAnchor) {
-        super.init()
-        
-        geometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
-        
-        let planeMaterial = SCNMaterial()
-        planeMaterial.diffuse.contents = UIColor.blue.withAlphaComponent(0.5)
-        geometry?.materials = [planeMaterial]
-        
-        let x = CGFloat(anchor.center.x)
-        let y = CGFloat(anchor.center.y)
-        let z = CGFloat(anchor.center.z)
-        
-        position = SCNVector3(x,y,z)
-        eulerAngles.x = -.pi / 2
-    }
-    func update(anchor: ARPlaneAnchor) {
-           
-           (geometry as! SCNPlane).width = CGFloat(anchor.extent.x)
-           (geometry as! SCNPlane).height = CGFloat(anchor.extent.z)
-           
-           let x = CGFloat(anchor.center.x)
-           let y = CGFloat(anchor.center.y)
-           let z = CGFloat(anchor.center.z)
-           position = SCNVector3(x,y,z)
-    }
-}
-
-//ViewContoller に必要な機能.
-//viewDid_load
-//ボタンの制御
-
-
+import AudioToolbox
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-    var deviceRot=SCNVector3(0,0,0)
-    var deviceAcc=SCNVector3(0,0,0)
-    var camera=SCNNode()
-    let motionManager = CMMotionManager()
-    let game = GameManager()
-    let visual = Visualizer()
-    
-    func startSensorUpdates(intervalSeconds:Double) {
-        motionManager.deviceMotionUpdateInterval = intervalSeconds
-        if motionManager.isDeviceMotionAvailable{
-            motionManager.startDeviceMotionUpdates(
-            to: OperationQueue.current!,
-            withHandler: {(motion:CMDeviceMotion?, error:Error?) in
-                self.setRot(deviceMotion: motion!)
-                self.setAcc(deviceMotion: motion!)
-            })
-        }
-    }
-    // デバイスの加速度を返す関数
-    func setAcc(deviceMotion: CMDeviceMotion){
-        let x=deviceMotion.userAcceleration.x
-        let y=deviceMotion.userAcceleration.y
-        let z=deviceMotion.userAcceleration.z
-        deviceAcc=SCNVector3(x,y,z)
-    }
-    // デバイスの角速度を返す関数
-    func setRot(deviceMotion: CMDeviceMotion){
-        //GameManagerのフィールド変数に加速度を渡す(SCNVector3)
-        let x=deviceMotion.rotationRate.x
-        let y=deviceMotion.rotationRate.y
-        let z=deviceMotion.rotationRate.z
-        deviceRot=SCNVector3(x,y,z)
-    }
-    @IBAction func startCasting(_ sender: UIButton) {
-        startSensorUpdates(intervalSeconds: 0.01)
-    }
-        
-    @IBAction func endCasting(_ sender: UIButton) {
-        motionManager.stopAccelerometerUpdates()
-    }
-    
+
     @IBOutlet var sceneView: ARSCNView!
     
+    //AR Resourcesに目的の画像が埋め込まれている
+    let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main)
+    
     var existence = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Visualizerがインスタンス生成
+        
+        let scene = SCNScene()
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -163,51 +31,214 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Set the scene to the view
-        sceneView.scene = visual.scene
+        sceneView.scene = scene
         
         // デバックオプション
         sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        
+        // tap
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapView))
+        sceneView.addGestureRecognizer(tapGesture)
+        
+        // longPress
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPressView))
+        sceneView.addGestureRecognizer(longPressGesture)
+        
         // オムニライトを追加
-        /*
         let lightNode = SCNNode()
         lightNode .light = SCNLight()
         lightNode .light!.type = .omni
         lightNode .position = SCNVector3(x: 0, y: 10, z: 10)
-        visual.scene.rootNode.addChildNode(lightNode )
-        */
+        scene.rootNode.addChildNode(lightNode)
     }
     
-
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
+        // Create a session configuration
+        /*
+        let configuration = ARImageTrackingConfiguration()
+        configuration.trackingImages = referenceImages!
+         */
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        
-        // 水平表示
-        configuration.planeDetection = .horizontal
+        configuration.planeDetection = [.horizontal]
+
+        // Run the view's session
+        sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 
+    func makeLine(_ from:SCNVector3,_ to:SCNVector3){
+        if let oldLineObject = sceneView.scene.rootNode.childNode(withName:"line",recursively: true){
+            oldLineObject.removeFromParentNode()
+            }
+        let source = SCNGeometrySource(vertices: [from, to])
+        let indices: [Int32] = [0, 1]
+        let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+        let line = SCNGeometry(sources: [source], elements: [element])
+        line.firstMaterial?.lightingModel = SCNMaterial.LightingModel.blinn
+        let dummyLine = SCNNode(geometry: line)
+        dummyLine.geometry?.firstMaterial?.emission.contents = UIColor.white
+        dummyLine.name="line"
+        sceneView.scene.rootNode.addChildNode(dummyLine)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+        guard let camera = sceneView.pointOfView else {
+            return
+        }
+        let cameraPos = SCNVector3Make(0, 0, -0.5)
+        let position = camera.convertPosition(cameraPos, to: nil)
+        if existence{
+            if let objNode = sceneView.scene.rootNode.childNode(withName: "Float", recursively: true){
+                objNode.position = position
+            }
+        }
+        if let objNode = sceneView.scene.rootNode.childNode(withName: "Float", recursively: true){
+            let cubePosi = objNode.position
+            let homePosi = SCNVector3(position.x,position.y+0.3,position.z)
+            makeLine(homePosi,cubePosi)
+            }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+            // 平面ジオメトリを作成
+            //let geometry = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x),
+        height: CGFloat(planeAnchor.extent.z))
+        geometry.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.5)
+
+        // 平面ジオメトリを持つノードを作成
+        let planeNode = SCNNode(geometry: geometry)
+            //x-z平面に合わせる
+        planeNode.eulerAngles.x = -Float.pi/2
+        //planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1, 0, 0)
+        
+        DispatchQueue.main.async(execute: {
+            // 検出したアンカーに対応するノードに子ノードとして持たせる
+            node.addChildNode(planeNode)
+        })
+    }
+    
+    // 更新されたとき
+   func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+       guard let planeAnchor = anchor as? ARPlaneAnchor else {fatalError()}
+
+       DispatchQueue.main.async(execute: {
+           // 平面ジオメトリのサイズを更新
+           for childNode in node.childNodes {
+               guard let plane = childNode.geometry as? SCNPlane else {continue}
+               plane.width = CGFloat(planeAnchor.extent.x)
+               plane.height = CGFloat(planeAnchor.extent.z)
+               break
+           }
+       })
+   }
+    
+    @objc func tapView(sender: UIGestureRecognizer) {
+        existence = false
+        
+        if let objNode = sceneView.scene.rootNode.childNode(withName: "Float", recursively: true){
+    
+            let location = sender.location(in: sceneView)
+            let hitTestResult = sceneView.hitTest(location, types: .existingPlaneUsingGeometry)
+        
+            if let result = hitTestResult.first {
+                // オブジェクトを飛ばす
+                let target = SCNVector3Make(
+                    result.worldTransform.columns.3.x,
+                    result.worldTransform.columns.3.y + 0.1,
+                    result.worldTransform.columns.3.z)
+                let action = SCNAction.move(to: target, duration: 1)
+                objNode.runAction(action)
+            }
+        }
+    }
+    
+    @objc func longPressView(sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            let location = sender.location(in: sceneView)
+            let hitTest  = sceneView.hitTest(location)
+            if let result = hitTest.first  {
+                if result.node.name == "Float"
+                {
+                    result.node.removeFromParentNode();
+                }
+            }
+        }
+    }
+    
+    @IBAction func DeleteKey(_ sender: Any) {
+        // sessionを停止
+        sceneView.session.pause()
+        // 全てのNodeに対して処理を行う
+        sceneView.scene.rootNode.enumerateChildNodes {(node, _) in
+        // Nodeを削除
+        node.removeFromParentNode()
+        }
+        // sessionを再開
+        sceneView.session.run(ARWorldTrackingConfiguration())
+    }
+    
+    
+    @IBAction func SettingButton(_ sender: Any) {
+        // キャスト音声：1001
+        //AudioServicesPlaySystemSoundWithCompletion(1001) {}
+        
+        let soundUrl = Bundle.main.url(forResource: "nc2039", withExtension: "mp3")
+        //let soundUrl = Bundle.main.url(forResource: "nc155740", withExtension: "wav")
+        //カスタムサウンドのサウンドIDを保存するための変数
+        var soundID: SystemSoundID = 0
+        //サウンドIDを取得
+        AudioServicesCreateSystemSoundID(soundUrl! as CFURL, &soundID)
+        //取得したサウンドIDを指定して再生
+         AudioServicesPlaySystemSoundWithCompletion(soundID) {}
+        
+        if existence{
+            if let objNode = sceneView.scene.rootNode.childNode(withName: "Float", recursively: true){
+                
+                let homePosi = SCNVector3(0,0,-0.5)
+                let action = SCNAction.move(to: homePosi, duration: 1)
+                objNode.runAction(action)
+            }
+        }else{
+        
+            // Cubeを作成
+            // let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+            /*
+            let obj = SCNSphere(radius: 0.05)
+            let objNode = SCNNode(geometry: obj)
+            objNode.name = "obj"
+            
+            
+            // Cubeのマテリアルを設定
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.red
+            material.diffuse.intensity = 0.8;
+            objNode.geometry?.materials = [material]
+            */
+
+
+            let floatScene = SCNScene(named: "Float.scn", inDirectory: "art.scnassets")
+            if let objNode = floatScene?.rootNode.childNode(withName: "Float", recursively: true){
+                objNode.scale = SCNVector3(0.01, 0.01, 0.01)
+                // Cubeの座標を設定
+                objNode.position = SCNVector3(0,0,-0.5)
+                sceneView.scene.rootNode.addChildNode(objNode)
+                existence = true
+            }
+        }
+    }
+    @IBAction func Back(_ sender: Any) {
+        self.performSegue(withIdentifier: "toMenu", sender: nil)
+    }
+    
 }
